1311931fef2afc04e9df3f966fe7c65268872b6d
Akshatha Hebbar <akshathahebbar1234@gmail.com>
2023-02-17 06:26:38 -0800

    Cors headers

--- HelixServer/settings.py
+++ HelixServer/settings.py
@@ -39,14 +39,21 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'rest_framework',
     # Questions Application
-    'questions.apps.QuestionsConfig'
+    'questions.apps.QuestionsConfig',
+    'corsheaders',
 ]
 
 ALLOWED_HOSTS = ['*']
 
+CORS_ALLOWED_ORIGINS = [
+"http://localhost:3000",
+"http://127.0.0.1:3000"
+]
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
