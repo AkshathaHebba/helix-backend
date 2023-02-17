929004293518bd10d22eeb62e003b4a4cf21b705
Akshatha Hebbar <akshathahebbar1234@gmail.com>
2023-02-17 03:35:21 -0800

    CSRF Check

--- HelixServer/settings.py
+++ HelixServer/settings.py
@@ -112,6 +112,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+## https://docs.djangoproject.com/en/4.0/ref/settings/#csrf-trusted-origins
+CSRF_TRUSTED_ORIGINS = ['https://agile-earth-88465.herokuapp.com/','https://*.127.0.0.1','http://*.127.0.0.1']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
